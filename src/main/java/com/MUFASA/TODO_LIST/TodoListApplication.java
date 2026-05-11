98643abf93e21e1bc4eadf2fd24e1c6fe242a341
Oussama-Ait-Youss <alikhay815@gmail.com>
2026-05-11 17:03:48 +0100

    first push

---
+++ src/main/java/com/MUFASA/TODO_LIST/TodoListApplication.java
@@ -0,0 +1,13 @@
+package com.MUFASA.TODO_LIST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApplication.class, args);
+	}
+
+}

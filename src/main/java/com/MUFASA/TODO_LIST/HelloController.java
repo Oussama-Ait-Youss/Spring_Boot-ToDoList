42810c2f60f6ac520e84f0f6cb80e67ad1b31976
Oussama-Ait-Youss <alikhay815@gmail.com>
2026-05-11 17:27:07 +0100

    create the first controller just print a string

---
+++ src/main/java/com/MUFASA/TODO_LIST/HelloController.java
@@ -0,0 +1,13 @@
+package com.MUFASA.TODO_LIST;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world this is spring boot running...";
+    }
+}
